83940d8431c5612c7dee3baab8c86a271728a239
priyanshusinghrjs <priyanshurjs0109@gmail.com>
2023-06-23 18:02:23 +0530

    docker & swagger error

---
+++ src/main/java/com/stackroute/retail_store/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.stackroute.retail_store.config;
+
+/**
+ * @author Priyanshu Singh
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+// @Bean
+// public Docket api() {
+//  return new Docket(DocumentationType.SWAGGER_2)
+//          .select()
+//          .apis(RequestHandlerSelectors.basePackage("com.example.yourpackage"))
+//          .paths(PathSelectors.any())
+//          .build()
+//          .apiInfo(apiInfo());
+// }
+//
+// private ApiInfo apiInfo() {
+//  return new ApiInfoBuilder()
+//          .title("Your API Documentation")
+//          .description("Description of your API")
+//          .version("1.0.0")
+//          .build();
+// }
+}
